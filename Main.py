465d31dcf7b4e1cf96a176c9362a96465228a1fa
Jared <Jared@aarealestategroup.com>
2019-11-15 08:24:34 -0500

    Newest commit

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@
 def main(msg):
 // testing
 // Print a Message
+// Another comment
     print(msg)
     main("Hello People")
     print("ho")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-pi-example
 This is an example project to demonstrate the ue of git on ubuntuqwerqwerqwer!
+Make a change
