8650139a534b0487cd733987476b34a528bd8d18
Jared <Jared@aarealestategroup.com>
2019-11-15 08:29:54 -0500

    new branch

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
 #!/usr.bin.python3
 def main(msg):
-// testing
-// Print a Message
-// Another comment
+//   testing
+//  Print a Message
+//  Another comment
+//  no longer need comments
+
+    print("ho")
+def secondary():
+    print("New function")
+
     print(msg)
     main("Hello People")
-    print("ho")
