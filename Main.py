071c1f7c33c9fd0704c6e544529a3d7ed0888b5e
Jared <Jared@aarealestategroup.com>
2019-11-16 15:52:13 -0500

    updated comments

--- Main.py
+++ Main.py
@@ -1,13 +1,12 @@
 #!/usr.bin.python3
 def main(msg):
-//   testing
-//  Print a Message
-//  Another comment
-//  no longer need comments
+# testing
+#  Print a Message
+# Another comment
+#  no longer need comments
 
     print("ho")
 def secondary():
     print("New function")
 
     print(msg)
-    main("Hello People")
